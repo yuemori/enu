7e0210dca7619bdc30f3b16402e7b02807bbe92a
yuemori <moonoverwalker@gmail.com>
2023-01-09 04:57:22 +0900

    Fix sliceEnumerator

--- enumerator.go
+++ enumerator.go
@@ -1,5 +1,7 @@
 package enu
 
+import "sync"
+
 func From[T any](collection []T) *Enumerator[T] {
 	return New[T](newSliceEnumerator(collection))
 }
@@ -15,6 +17,7 @@ func (e *Enumerator[T]) ToMap() map[int]T {
 type sliceEnumerator[T any] struct {
 	collection []T
 	index      int
+	mu         sync.Mutex
 }
 
 func newSliceEnumerator[T any](collection []T) *sliceEnumerator[T] {
@@ -23,6 +26,9 @@ func newSliceEnumerator[T any](collection []T) *sliceEnumerator[T] {
 
 func (e *sliceEnumerator[T]) Stop() {}
 func (e *sliceEnumerator[T]) Next() (T, bool) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	if len(e.collection) > e.index {
 		item := e.collection[e.index]
 		e.index += 1
